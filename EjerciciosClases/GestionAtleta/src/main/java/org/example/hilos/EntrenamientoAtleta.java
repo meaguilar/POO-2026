cee4ba3b72c78575afcd646b9cdaa15a299a8da9
Marlen <meaguilar@uca.edu.sv>
2026-05-11 15:42:30 -0600

    docs: agregar comentarios sobre las indicaciones a seguir en el salon de clases

---
+++ EjerciciosClases/GestionAtleta/src/main/java/org/example/hilos/EntrenamientoAtleta.java
@@ -0,0 +1,28 @@
+package org.example.hilos;
+
+// Implementar de la interfaz Runnable
+public class EntrenamientoAtleta implements Runnable{
+
+    // Crear el atributo que referencia a la clase atleta
+
+    // Constructor
+
+    //Sobreescribir el metodo run()
+    @Override
+    public void run() {
+
+        // Texto que imprima quien esta desarrollando el entrenamiento
+
+
+        // Ejecuta la lógica del atleta del metodo entrenar (usa el plan internamente)
+
+        // Sumar  la duración total del plan de entrenamiento
+
+
+        // Simulación del  tiempo total del entrenamiento
+
+
+        // Imprimir el entrenamiento ha finalizado por el atleta
+    }
+
+}
---
+++ EjerciciosClases/GestionAtleta/src/main/java/org/example/hilos/EvaluacionEntrenamiento.java
@@ -0,0 +1,19 @@
+package org.example.hilos;
+
+
+// Implementar de la interfaz Runnable
+public class EvaluacionEntrenamiento {
+
+    // Crear el atributo que referencia a la clase atleta y el entrenador
+
+    // Constructor
+
+    //Sobre escribir el metodo run()
+
+    // Imprimir nombre del entrenador y el atleta que esta evaluando
+
+    // Simula evaluación cada 5 segundos por la cantidad de ejercicios
+
+    // Texto que indica que el entrenador esta generando la retroalimentacion
+
+}

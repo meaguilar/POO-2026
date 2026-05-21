4687debdb252f236ab136a89faac0532e534cfa5
Marlen <meaguilar@uca.edu.sv>
2026-05-21 10:41:55 -0600

    feat: agregar validacion sobre la lista de atleta asignados a entrenador

--- EjerciciosClases/GestionAtleta/src/main/java/org/example/hilos/EvaluacionEntrenamiento.java
+++ EjerciciosClases/GestionAtleta/src/main/java/org/example/hilos/EvaluacionEntrenamiento.java
@@ -26,28 +26,36 @@ public class EvaluacionEntrenamiento implements Runnable {
     @Override
     public void run() {
 
-        // Imprimir nombre del entrenador y el atleta que esta evaluando
-        System.out.println("\nEntrenador responsable: " + entrenador.getNombre());
-        System.out.println("Atleta entrenando: " + entrenador.obtenerAtletaPorNombre(atleta).getNombre());
+        Atleta atletaEncontrado = entrenador.obtenerAtletaPorNombre(atleta);
 
-        // Simula evaluación cada 5 segundos sumados al valor en minutos de cada ejercicios
-        try {
-            for(var ejerciciosDuracion : atleta.getPlanActual().getEjerciciosDuracion().entrySet()) {
-                System.out.println("Evaluando .... " + ejerciciosDuracion.getKey());
-                // Simulación de la evaluacion 5 segundos mas el tiempo de cada ejercicio
-                Thread.sleep((long)((ejerciciosDuracion.getValue() * 60 * 1000) + 5000));
+        if (atletaEncontrado != null) {
+
+            String atletaAsignado = atletaEncontrado.getNombre();
+
+            // Imprimir nombre del entrenador y el atleta que esta evaluando
+            System.out.println("\nEntrenador responsable: " + entrenador.getNombre());
+            System.out.println("Atleta entrenando: " + atletaAsignado);
+
+            // Simula evaluación cada 5 segundos sumados al valor en minutos de cada ejercicios
+            try {
+                for (var ejerciciosDuracion : atleta.getPlanActual().getEjerciciosDuracion().entrySet()) {
+                    System.out.println("Evaluando .... " + ejerciciosDuracion.getKey());
+                    // Simulación de la evaluacion 5 segundos mas el tiempo de cada ejercicio
+                    Thread.sleep((long) ((ejerciciosDuracion.getValue() * 60 * 1000) + 5000));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Evaluación interrumpida");
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Evaluación interrumpida");
+            // Texto que indica que el entrenador esta generando la retroalimentacion
+            System.out.println("Generando retroalimentación .......");
+            // invocar metodos  firmar y guardar
+            reporte.firmarReporte(" atleta: [" + atletaAsignado + "]  - entrenador: [" + entrenador.getNombre() + "]\n");
+            gestorReporte.guardarReportes(reporte);
+        }else {
+            System.out.println("Atleta no encontrado en la lista del entrenador: " + entrenador.getNombre());
         }
-
-        // Texto que indica que el entrenador esta generando la retroalimentacion
-        System.out.println("Generando retroalimentación .......");
-        // invocar metodos  firmar y guardar
-        reporte.firmarReporte(" atleta: [" + atleta.getNombre() + "]  - entrenador: [" + entrenador.getNombre() + "]\n");
-        gestorReporte.guardarReportes(reporte);
-
     }
 }

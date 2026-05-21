d12cb049211805dfc571a51607f77c3514a6b3d8
Marlen <meaguilar@uca.edu.sv>
2026-05-21 09:05:13 -0600

    feat: agregar nuevo atributo para la impresion en consola

--- EjerciciosClases/GestionAtleta/src/main/java/org/example/patrones/estrategia/visualizacion/VisualizacionCompleta.java
+++ EjerciciosClases/GestionAtleta/src/main/java/org/example/patrones/estrategia/visualizacion/VisualizacionCompleta.java
@@ -5,8 +5,10 @@ public class VisualizacionCompleta implements EstrategiaVisualizacion {
     @Override
     public void visualizar(Reporte reporte){
         System.out.println("\nReporte completo");
-        System.out.println("Asunto: " + reporte.getAsunto());
         System.out.println("Fecha: " + reporte.getFecha());
+        System.out.println("Asunto: " + reporte.getAsunto());
+        System.out.println("Firma: " + reporte.getFirma());
+
     }
 
 }
